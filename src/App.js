f3b32e945e236858503b0ef7120ca24af2b5874c
Harys <harysrandrianomenjanahary@gmail.com>
2024-08-12 19:50:47 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center text-blue-500">
+      <h1 className="text-3xl font-bold">
+        Bienvenue à mon site de voyage!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
